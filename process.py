8a791c5f9a4851d5732b43a9c2f8f881ec5d30ee
galbraithmedia1 <59584919+galbraithmedia1@users.noreply.github.com>
2022-02-18 09:23:16 -0700

    part 1 finished

--- process.py
+++ process.py
@@ -1,12 +1,12 @@
 log_file = open("um-server-01.txt")
 
 
-def sales_reports(log_file):
-    for line in log_file:
-        line = line.rstrip()
-        day = line[0:3]
-        if day == "Tue":
-            print(line)
+def sales_reports(log_file): #This line creates a function using def named sales_report and is passing in log_file
+    for line in log_file: # this is a for loop that loops through log_file and assigns the loop name to line
+        line = line.rstrip() #line.rstrip() will take out all any trailing characters
+        day = line[0:3] #assigns line index0 to index 3 to the varible day
+        if day == "Mon": #this line will only pull data where the day is assigned to Tue. 
+            print(line)#This like prints the loop assigned to line
 
 
-sales_reports(log_file)
+sales_reports(log_file)#calls the function and passes in log_file
